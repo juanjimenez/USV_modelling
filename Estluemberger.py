67e955a9d91024c7b70ebfec3d9f03ed36487e1e
Juan Jiménez <juan.jimenez@fis.ucm.es>
2024-03-18 11:14:59 +0100

    ultimos cambios en el observador de Luemberger

--- Estluemberger.py
+++ Estluemberger.py
@@ -46,18 +46,18 @@ def USV(t,p,p0,mus,Ks,C,E,campvw,campo):
     k = Ks[1]
     L = Ks[2]
     wt = campvw(p[0:2],t ,campo)
-    R = np.reshape(p[8:12],[2,2])
-    ep =p0-p[0:2] #position error
+    R = np.reshape(p[8:12],[2,2]) #USV heading 
+    ep =p0-p[0:2] #position error p0 is the desired equilibrium point to down the probe 
     
     nD = np.linalg.norm(ep)
-    Dd = ep/(nD+ (nD==0)) #heading towards the origin
+    Dd = ep/(nD+ (nD==0)) #heading towards the origin (equilibrium point)
     #
     st= Dd.T.dot(E.dot(R.dot(ux)))
     #
     ct = Dd.T.dot(R.dot(ux))
     #ev = p[2:4]-p[6:8] #velocity error
     #torque to be applied
-    #Tau = k1*st*np.sign(ct)
+    #Tau = kr*st*(np.sign(ct)+1*(ct==0))#la condición es para evitar el punto de eq inestable
     Tau = kr*st/ct
     
     Pc =R.T.dot(p[0:2]) #posiciones a ejes cuerpo 
@@ -66,17 +66,17 @@ def USV(t,p,p0,mus,Ks,C,E,campvw,campo):
     Pcdothat =R.T.dot(p[6:8]) #velocidades a ejes cuerpo estimadas
     
     #estados de la planta reducida
-    Pr =np.array([Pc[0],Pcdot[0]])
+    Pr =np.array([Pc[0],Pcdot[0]]) #solo controlo la 
     #estados del estimador reducido px y vx
     Prhat =np.array([Pchat[0],Pcdothat[0]]) 
     
     
-    F = -k[0,0:2].dot(Pchat) - k[0,2]*p[13] #fuera sacada del modelo lineal
+    F = -k[0,0:2].dot(Pchat) - k[0,2]*p[13] #fuerza sacada del modelo lineal
     
     #USV displacement
     dotp[0:2] = p[2:4] 
-    dotp[2:4] = R.dot(ux*F) - R.dot(mu.dot(R.T.dot(p[2:4])- wt))
-    
+    dotp[2:4] = R.dot(ux*F) - R.dot(mu.dot(R.T.dot(p[2:4]- wt)))
+    #dotp[2:4] = R.dot(ux*F) - R.dot(mu.dot(R.T.dot(p[2:4])))
     #USV rotation
     Td = (F + Tau)/2
     Ti = (F - Tau)/2
@@ -90,10 +90,10 @@ def USV(t,p,p0,mus,Ks,C,E,campvw,campo):
     dotpr = L.dot(C.dot(Pr))
     dotprhat =L.dot(C.dot(Prhat))
     
-    dotp[4:6] = R.dot(ux*dotpr[0]) + p[6:8] - R.dot(ux*dotprhat[0])  
-    dotp[6:8] = R.dot(ux*dotpr[1])+R.dot(ux*F) - R.dot(mu.dot(R.T.dot(p[6:8])))\
-    - R.dot(ux*dotprhat[1])
-    Pc = R.dot(p0-p[0:2]) #solo hacemos tracking de px en c cuerpo
+    dotp[4:6] = p[6:8] + R.dot(ux*(dotpr[0]-dotprhat[0]))  
+    dotp[6:8] = R.dot(ux*F) - R.dot(mu.dot(R.T.dot(p[6:8])))\
+    + R.dot(ux*(dotpr[1]- dotprhat[1]))
+    Pc = R.T.dot(p0-p[0:2]) #solo hacemos tracking de px en c cuerpo
     dotp[13] =Pc[0] 
     
     return(dotp)
@@ -107,7 +107,7 @@ p[1] ->> py
 p[2] ->> vx
 p[3] ->> vy
 p[4] ->> ^px (^. Estimator variables)
-p[5] ->> ^py
+p[5] ->> ^pydotp[2:4] = R.dot(ux*F) - R.dot(mu.dot(R.T.dot(p[2:4]- wt))1
 p[6] ->> ^vx
 p[7] ->> ^vy
 p[8] ->> R[0,0] (Rotation Matrix elements)
@@ -117,8 +117,8 @@ p[11] ->> R[0,1]
 p[12] ->> w (angular speed)
 p[13] ->> dev error de posición
 '''
-#thetai = np.arange(0,2*np.pi,np.pi/6) #initial USV heading
-thetai = [np.pi]
+#thetai = np.arange(0,2*np.pi,np.pi/8) #initial USV heading
+thetai = [np.pi/3]
 pini = np.zeros(14)
 r = 4 #distancia al origen a partir de la cual se prueba el algoritmo
 v = 0.5 #v inicial del usv, (modulo)
@@ -132,10 +132,10 @@ for theta in thetai:
     pini[5] = r*np.sin(theta)
     pini[6] = -v*np.cos(theta)
     pini[7] = -v*np.sin(theta)
-    pini[8] = -np.cos(theta)
-    pini[9] =  np.sin(theta)
-    pini[10] = -np.sin(theta)
-    pini[11] = -np.cos(theta)
+    pini[8] = np.cos(theta)
+    pini[9] =  -np.sin(theta)
+    pini[10] = np.sin(theta)
+    pini[11] = np.cos(theta)
     pini[12] = 0
     pini[13] = 0
     p0 = np.array([0,0]) #desired stabilization position
@@ -152,11 +152,11 @@ for theta in thetai:
     A = np.array([[0,1],[0,-mu[0,0]]])
     B = np.array([[0],[1]])
     C = np.array([[1,0]])
-    Poles = [-30,-20] #perhaps improve it with LQR¿?
+    Poles = [-3,-3] #perhaps improve it with LQR¿?
     L = ctrl.acker(A.T,C.T,Poles).T
     #y a continuación las del controlador, tambien simplificado (solo para p_x)
     #incluimos acción integral para tratar de controlar las corrientes 
-    Pols = [-0.1,-0.2,-1]
+    Pols = [-2,-2,-1]
     Amp = np.array([[0, 1, 0],[0, -mu[0][0], 0], [-1, 0, 0]])
     Bmp = np.array([[0],[1],[0]])
     K = ctrl.acker(Amp,Bmp,Pols)
@@ -166,8 +166,8 @@ for theta in thetai:
     
     E = np.array([[0,-1],[1,0]])
     #campo = [3*np.pi/4,0*np.pi/100,1,0.5]
-    campo = [np.pi/4,0*np.pi/100,1,0.5]
-    tfin = 200. #2000.#tiempo final del experimento
+    campo = [np.pi/3,0*np.pi/100,1,0.5]
+    tfin = 20. #2000.#tiempo final del experimento
     
     #_______integrador LSODA (stiff)___________________________________________
     USV(0,pini,p0,mus,Ks,C,E,campoux,campo)
@@ -179,11 +179,14 @@ for theta in thetai:
     pl.figure(1) #positions vs time
     pl.plot(sol.t,sol.y[0],'r')
     pl.plot(sol.t,sol.y[1],'k')
+    pl.plot(sol.t,sol.y[4],'g')
+    pl.plot(sol.t,sol.y[5],'b')
+    
     pl.xlabel('t(s.)')
     pl.ylabel('m')
     pl.legend(['$p_1$','$p_2$'])
     pl.title('$\\theta$ =%2.0f$^o$, r=%2.1fm., v=%2.1fm/s' %(theta*180/np.pi, r, v))
-    pl.savefig('./figures/pos_th%2.0f_r%dm_v%dms.eps' %(theta*180/np.pi, r, v))
+    pl.savefig('./figures/pos_th%2.0f_r%dm_v%dms.png' %(theta*180/np.pi, r, v))
     pl.figure(2) #positions vs time
     pl.plot(sol.t,sol.y[2],'r')
     pl.plot(sol.t,sol.y[3],'k')
@@ -191,7 +194,7 @@ for theta in thetai:
     pl.ylabel('m/s')
     pl.legend(['$\dot{p_1}$','$\dot{p_2}$'])
     pl.title('$\\theta$ =%2.0f$^o$, r=%2.1fm., v=%2.1fm/s' %(theta*180/np.pi, r, v))
-    pl.savefig('./figures/vel_th%2.0f_r%dm_v%dms.eps' %(theta*180/np.pi, r, v))
+    pl.savefig('./figures/vel_th%2.0f_r%dm_v%dms.png' %(theta*180/np.pi, r, v))
     # # pl.plot(t,w,'.b')
     # pl.figure(2) #velocities vs time
     # pl.plot(sol.t,sol.y[2],'.r')pl.figure(1) #positions vs time
@@ -201,7 +204,7 @@ for theta in thetai:
     pl.ylabel('rad/s')
     pl.legend(['$\omega$'])
     pl.title('$\\theta$ =%2.0f$^o$, r=%2.1fm., v=%2.1fm/s' %(theta*180/np.pi, r, v))
-    pl.savefig('./figures/omega_th%2.0f_r%dm_v%dms.eps' %(theta*180/np.pi, r, v))
+    pl.savefig('./figures/omega_th%2.0f_r%dm_v%dms.png' %(theta*180/np.pi, r, v))
     
     
     vertices = np.array([[0, 0.25],[0,-0.25],[1,0],[0,0.25]])
@@ -217,7 +220,7 @@ for theta in thetai:
     L = sol.t.shape[0]
     
     #drawing the ship atitude
-    step= 0.1
+    step= 0.001
     
     ind = [int(step*((i-(L/2)**(1/3))**3+L/2)) for i in range(L)\
            if step*((i-(L/2)**(1/3))**3+L/2) < L]
@@ -242,7 +245,7 @@ for theta in thetai:
     pl.xlabel('$p_1$(m.)')
     pl.ylabel('$p_2$(m.)')
     pl.title('$\\theta$ =%2.0f$^o$, r=%2.1fm., v=%2.1fm/s' %(theta*180/np.pi, r, v))
-    pl.savefig('./figures/disp_th%2.0f_r%dm_v%dms.eps' %(theta*180/np.pi, r, v))
+    pl.savefig('./figures/disp_th%2.0f_r%dm_v%dms.png' %(theta*180/np.pi, r, v))
     
     ux = np.array([1,0])
     dotp = np.zeros(13)
@@ -286,4 +289,4 @@ for theta in thetai:
     pl.xlabel('t(s.)')
     pl.ylabel('N.')
     pl.title('$\\theta$ =%2.0f$^o$, r=%2.1fm., v=%2.1fm/s' %(theta*180/np.pi, r, v))
-    pl.savefig('./figures/thruster_th%2.0f_r%dm_v%dms.eps' %(theta*180/np.pi, r, v))
+    pl.savefig('./figures/thruster_th%2.0f_r%dm_v%dms.png' %(theta*180/np.pi, r, v))
