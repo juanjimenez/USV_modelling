0d6155830b1675fc251892d5ed13724db6b5d7eb
juanjimenez <juan.jimenez@fis.ucm.es>
2024-04-04 20:53:01 +0200

    vel agua arreglado

--- vel_agua.py
+++ vel_agua.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as pl
 
 def campoux(p,t,campo):
    '''representa un campo uniforme en la direccion marcada por theta
-   #el campo alcanza un maximo en la recta y = tan(theta)*x y cae a cero segu
+   #el campo alcanza un maximo en la recta y = tan(theta)*x +b y cae a cero segu
    #nos alejamos de ella
    #p coordenadas en que se calcula el valor de la velocidad de arrastre
     el campo puede variar su orientacion con el tiempo si w no vale cero
@@ -32,7 +32,7 @@ def campoux(p,t,campo):
    w = campo[1]
    A = campo[2]
    S = campo[3]
-   
+   b = campo[4]
    #desrrotamos el punto. Solo necesito desrrotar la y
    pr = -p[0]*np.sin(theta+w*t) + p[1]*np.cos(theta+w*t)
    
@@ -40,28 +40,32 @@ def campoux(p,t,campo):
    
    
    #el campo en el punto lo ligamos a     
-   v=A*np.exp(-S*pr**2)
+   v=A*np.exp(-S*(pr+b)**2)
 
    wr = np.array([v*np.cos(theta+w*t),v*np.sin(theta+w*t)])
    return wr
    
-def pintacampoux(X,Y,campo,t=0):
+def pintacampoux(X,Y,campoux,campo,col,t=0):
     '''para pintar el campo con flechillas 
     X e Y son dos vectores de puntos sobre los que hacer un grid de puntos en 
     los que calcular y pintar las flechillas del campo.
     campo es la misma lista que hemos definido en la funcion campoux
     '''
     
-    theta = campo[0]
-    w = campo[1]
-    A = campo[2]
-    S = campo[3]
-   
+# =============================================================================
+#     theta = campo[0]
+#     w = campo[1]
+#     A = campo[2]
+#     S = campo[3]
+#     b = campo[4]   
+# =============================================================================
     Xm,Ym = np.meshgrid(X,Y)
-    Vm = A*np.exp(-S*Ym**2)
-    Xmr = Xm#Xm*np.cos(theta+w*t) - Ym*np.sin(theta+w*t)
-    Ymr = Ym#Xm*np.sin(theta+w*t) + Ym*np.cos(theta+w*t)
-    Wmx = Vm*np.cos(theta+w*t)
-    Wmy = Vm*np.sin(theta+w*t)
-    pl.quiver(Xmr,Ymr,Wmx,Wmy,color='r',width=1e-3)
+    p =[Xm,Ym]
+    Vm = campoux(p,0,campo)
+    #Ymr = -Xm*np.sin(theta+w*t) + Ym*np.cos(theta+w*t)
+    #Vm = A*np.exp(-S*(Ymr+b)**2)
+    #Wmx = Vm*np.cos(theta+w*t)
+    #Wmy = Vm*np.sin(theta+w*t)
+    
+    pl.quiver(Xm,Ym,Vm[0],Vm[1],color=col,width=1e-3)
             
